03fbcc1bc217ff0e5eb8bca46873bd5a93360c27
Savio Rony <savio_ls@hotmail.com>
2022-09-12 19:55:30 -0300

    Inciado projeto de estudos com spring batch

---
+++ estudo-batch/src/main/java/br/com/batch/estudobatch/EstudoBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.batch.estudobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoBatchApplication.class, args);
+	}
+
+}
---
+++ estudo-batch/src/test/java/br/com/batch/estudobatch/EstudoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batch.estudobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
